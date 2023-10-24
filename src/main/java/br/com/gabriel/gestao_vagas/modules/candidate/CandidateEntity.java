9aef1c411c01677440ca8a61a5d7b3b295b5c3a2
Gabriel Desktop <guiegabriel321@gmail.com>
2023-10-24 20:12:33 -0300

    feat - Adicionando validação de error e tratamento nos erros

--- src/main/java/br/com/gabriel/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/gabriel/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,7 +1,11 @@
 package br.com.gabriel.gestao_vagas.modules.candidate;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -11,8 +15,14 @@ public class CandidateEntity {
     private UUID id;
 
     private String name;
+
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço!")
     private String username;
+
+    @Email(message = "O campo deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 6, max = 16)
     private String password;
     private String description;
     private String curriculum;
